0a1998cb655788cc9911fb378a9c63f0c5511fc6
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2023-12-15 11:27:11 +0100

    Prepare API calls

--- news-app/src/components/News.js
+++ news-app/src/components/News.js
@@ -2,14 +2,29 @@ import { Component } from "react";
 import NewsSingle from "./NewsSingle";
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/top-headlines?language=en&apiKey=${process.env.REACT_API_KEY}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.state({ news: data.articles }))
+      .catch((error) => console.error(error));
+  }
+
   renderItems() {
-    return this.props.items.map((item) => (
+    return this.props.news.map((item) => (
       <NewsSingle key={item.id} item={item} />
     ));
   }
 
   render() {
-    return <ul>{this.renderItems()}</ul>;
+    return <div className="row">{this.renderItems()}</div>;
   }
 }
 
--- news-app/src/components/NewsSingle.js
+++ news-app/src/components/NewsSingle.js
@@ -1,5 +1,7 @@
-const NewsSingle = (item) => {
-  <li>{item}</li>;
+const NewsSingle = ({ item }) => {
+  <div className="card-content">
+    <h2>{item.title}</h2>
+  </div>;
 };
 
 export default NewsSingle;
