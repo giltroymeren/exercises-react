b3fb96058ab3d667d7af090c9959aa2757004806
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-17 19:35:26 +0100

    Init ReactJS with TS

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 4000,
+    hot: false,
+    liveReload: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+};
