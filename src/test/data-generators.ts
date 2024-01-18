fcbd8e7abd93229f08f00efb416c1f42ff7fb3b0
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-18 20:39:03 +0100

    Add fakers

---
+++ src/test/data-generators.ts
@@ -0,0 +1,34 @@
+import { faker } from "@faker-js/faker";
+
+type Overrides = Record<string, any>;
+
+export const addressGenerator = (overrides?: Overrides) => ({
+  street: faker.location.street(),
+  suite: faker.location.secondaryAddress(),
+  city: faker.location.city(),
+  zipcode: faker.location.zipCode(),
+  geo: {
+    lat: faker.location.latitude(),
+    lng: faker.location.longitude(),
+  },
+  ...overrides,
+});
+
+export const companyGenerator = (overrides?: Overrides) => ({
+  name: faker.company.name(),
+  catchPhrase: faker.company.catchPhrase(),
+  bs: `${faker.company.buzzAdjective()} ${faker.company.buzzNoun()}`,
+  ...overrides,
+});
+
+export const userGenerator = (overrides?: Overrides) => ({
+  id: faker.string.uuid(),
+  name: faker.person.fullName(),
+  username: faker.internet.userName(),
+  email: faker.internet.email(),
+  address: addressGenerator(),
+  phone: faker.phone.number(),
+  website: faker.internet.domainName(),
+  company: companyGenerator(),
+  ...overrides,
+});
