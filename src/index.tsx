a1ff5a72111ab19670ec34b7a65a624ccd439370
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-19 13:23:23 +0100

    Setup react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,18 @@ import "./index.less";
 import App from "./App";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./routes/NotFound";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd";
+
+const NotFound = () => (
+  <div data-test="container-404">
+    <h3>Page not found</h3>
+    <Button href="/">Back to Home</Button>
+  </div>
+);
+
+export default NotFound;
