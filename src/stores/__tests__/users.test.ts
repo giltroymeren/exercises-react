71ccdd09720fc0d603a4812eda8298e45fd1a366
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-19 14:14:09 +0100

    Setup testing

--- src/stores/__tests__/users.test.ts
+++ src/stores/__tests__/users.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useUsersStore } from "../users";
 import { User } from "../../features/users/types";
 
@@ -6,4 +6,34 @@ test("should be able to add users", () => {
   const { result } = renderHook(() => useUsersStore());
 
   expect(result.current.users.length).toBe(0);
+
+  const newUser: User = {
+    id: 100,
+    name: "",
+    username: "",
+    email: "",
+    address: {
+      street: "",
+      suite: "",
+      city: "",
+      zipcode: "",
+      geo: {
+        lat: "",
+        lng: "",
+      },
+    },
+    phone: "",
+    website: "",
+    company: {
+      name: "",
+      catchPhrase: "",
+      bs: "",
+    },
+  };
+
+  act(() => {
+    result.current.add(newUser);
+  });
+
+  expect(result.current.users.length).toBe(1);
 });
--- src/stores/users.ts
+++ src/stores/users.ts
@@ -10,6 +10,7 @@ type State = {
 type Actions = {
   setAll: (users: User[]) => void;
   getAll: (state: State) => User[];
+  add: (user: User) => void;
   // TODO getById, add, edit, delete
 };
 
@@ -22,6 +23,10 @@ export const useUsersStore = create<State & Actions>((set) => ({
   ...initialState,
   setAll: (users) => set({ users }),
   getAll: (state: State) => state.users,
+  add: (user) =>
+    set((state) => ({
+      users: [...state.users, user],
+    })),
   reset: () => {
     set(initialState);
   },
