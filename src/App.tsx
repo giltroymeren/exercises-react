3ed84b01663f375894c5b158115e762053c4c93d
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-19 19:09:46 +0100

    Add MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import * as React from "react";
-import Layout, { Content, Footer } from "antd/es/layout/layout";
 import UsersList from "./features/users/components/UsersList";
+import MainLayout from "./components/Layout/MainLayout";
 
 const App = () => {
   return (
-    <Layout>
-      <h1>Customer Phone Book App</h1>
-      <Layout>
-        <Content>
-          <UsersList />
-        </Content>
-      </Layout>
-      <Footer>
-        <a
-          href="https://giltroymeren.github.io/"
-          target="_blank"
-          rel="noreferrer nofollow"
-        >
-          Troy Meren
-        </a>{" "}
-        &copy; 2024
-      </Footer>
-    </Layout>
+    <MainLayout>
+      <UsersList />
+    </MainLayout>
   );
 };
 
---
+++ src/components/Layout/MainLayout.tsx
@@ -0,0 +1,40 @@
+import { Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const HeaderSection = () => (
+  <div data-test="section-header">
+    <Header>
+      <span>Customer Phone Book App</span>
+    </Header>
+  </div>
+);
+
+const FooterSection = () => (
+  <Footer data-test="section-footer">
+    <a
+      href="https://giltroymeren.github.io/"
+      target="_blank"
+      rel="noreferrer nofollow"
+    >
+      Troy Meren
+    </a>{" "}
+    &copy; 2024
+  </Footer>
+);
+
+const MainLayout = ({ children }: Props) => (
+  <Layout>
+    <HeaderSection />
+    <Layout data-test="section-body">
+      <Content>{children}</Content>
+    </Layout>
+    <FooterSection />
+  </Layout>
+);
+
+export default MainLayout;
