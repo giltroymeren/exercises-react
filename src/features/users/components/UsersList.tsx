b9f724e0f1f9bd3ef3df5f324489a0b933048892
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-20 11:35:25 +0100

    Impl user validation

--- src/features/users/components/UsersList.tsx
+++ src/features/users/components/UsersList.tsx
@@ -39,6 +39,19 @@ const UsersList = () => {
 
   const [form] = Form.useForm();
   const formValues = Form.useWatch<NewUser>([], form);
+  const [isFormValid, setIsFormValid] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log(`isFormValid`, isFormValid);
+    form.validateFields({ validateOnly: true }).then(
+      () => {
+        setIsFormValid(true);
+      },
+      () => {
+        setIsFormValid(false);
+      }
+    );
+  }, [formValues]);
 
   React.useEffect(() => {
     if (usersQuery.data && !fetched) {
@@ -158,32 +171,61 @@ const UsersList = () => {
               data-test="form-create"
               scrollToFirstError
               onFinish={handleSubmit}
+              name="validateOnly"
             >
-              <Form.Item label="Name" name="name">
+              <Form.Item label="Name" name="name" rules={[{ required: true }]}>
                 <Input name="name" />
               </Form.Item>
-              <Form.Item label="Username" name="username">
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, min: 6 }]}
+              >
                 <Input name="username" />
               </Form.Item>
-              <Form.Item label="Email" name="email">
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[{ required: true, min: 6 }]}
+              >
                 <Input name="email" />
               </Form.Item>
-              <Form.Item label="Website" name="website">
+              <Form.Item
+                label="Website"
+                name="website"
+                rules={[{ required: true }]}
+              >
                 <Input name="website" />
               </Form.Item>
-              <Form.Item label="Phone" name="phone">
+              <Form.Item
+                label="Phone"
+                name="phone"
+                rules={[{ required: true, min: 6 }]}
+              >
                 <Input name="phone" />
               </Form.Item>
-              <Form.Item label="Address" name="address">
+              <Form.Item
+                label="Address"
+                name="address"
+                rules={[{ required: true }]}
+              >
                 <TextArea name="address" />
               </Form.Item>
-              <Form.Item label="Company Name" name="company">
+              <Form.Item
+                label="Company Name"
+                name="company"
+                rules={[{ required: true }]}
+              >
                 <Input name="company" />
               </Form.Item>
               <Form.Item>
                 <Space>
                   <Button onClick={closeDrawer}>Cancel</Button>
-                  <Button type="primary" htmlType="submit">
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    disabled={!isFormValid}
+                  >
                     Create User
                   </Button>
                 </Space>
