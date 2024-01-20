57680ef19ba83a69f8615fa4613e6b7e4ea66a02
Troy Meren <giltroymeren@Troys-MacBook-Pro.local>
2024-01-20 11:42:43 +0100

    Add UserForm

---
+++ src/components/Forms/UserForm.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import { Button, Form, Input, Space } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { NewUser } from "../../features/users";
+
+type Props = {
+  handleSubmit: (values: any) => void;
+  onCancel: () => void;
+};
+
+const UserForm = ({ handleSubmit, onCancel }: Props) => {
+  const [form] = Form.useForm();
+  const formValues = Form.useWatch<NewUser>([], form);
+  const [isFormValid, setIsFormValid] = React.useState(false);
+
+  React.useEffect(() => {
+    form.validateFields({ validateOnly: true }).then(
+      () => {
+        setIsFormValid(true);
+      },
+      () => {
+        setIsFormValid(false);
+      }
+    );
+  }, [formValues]);
+
+  return (
+    <Form
+      layout="vertical"
+      autoComplete="off"
+      data-test="form-create"
+      scrollToFirstError
+      onFinish={handleSubmit}
+      name="validateOnly"
+    >
+      <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+        <Input name="name" />
+      </Form.Item>
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, min: 6 }]}
+      >
+        <Input name="username" />
+      </Form.Item>
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, min: 6 }]}
+      >
+        <Input name="email" />
+      </Form.Item>
+      <Form.Item label="Website" name="website" rules={[{ required: true }]}>
+        <Input name="website" />
+      </Form.Item>
+      <Form.Item
+        label="Phone"
+        name="phone"
+        rules={[{ required: true, min: 6 }]}
+      >
+        <Input name="phone" />
+      </Form.Item>
+      <Form.Item label="Address" name="address" rules={[{ required: true }]}>
+        <TextArea name="address" />
+      </Form.Item>
+      <Form.Item
+        label="Company Name"
+        name="company"
+        rules={[{ required: true }]}
+      >
+        <Input name="company" />
+      </Form.Item>
+      <Form.Item>
+        <Space>
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button type="primary" htmlType="submit" disabled={!isFormValid}>
+            Create User
+          </Button>
+        </Space>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default UserForm;
--- src/features/users/components/UsersList.tsx
+++ src/features/users/components/UsersList.tsx
@@ -20,6 +20,7 @@ import NiceModal from "@ebay/nice-modal-react";
 import SimpleModal from "../../../components/Elements/SimpleModal";
 import { PlusOutlined } from "@ant-design/icons";
 import TextArea from "antd/es/input/TextArea";
+import UserForm from "../../../components/Forms/UserForm";
 
 const UsersList = () => {
   const navigate = useNavigate();
@@ -36,22 +37,6 @@ const UsersList = () => {
     () => setIsDrawerOpen(false),
     [isDrawerOpen]
   );
-
-  const [form] = Form.useForm();
-  const formValues = Form.useWatch<NewUser>([], form);
-  const [isFormValid, setIsFormValid] = React.useState(false);
-
-  React.useEffect(() => {
-    console.log(`isFormValid`, isFormValid);
-    form.validateFields({ validateOnly: true }).then(
-      () => {
-        setIsFormValid(true);
-      },
-      () => {
-        setIsFormValid(false);
-      }
-    );
-  }, [formValues]);
 
   React.useEffect(() => {
     if (usersQuery.data && !fetched) {
@@ -165,72 +150,7 @@ const UsersList = () => {
               navigate("/");
             }}
           >
-            <Form
-              layout="vertical"
-              autoComplete="off"
-              data-test="form-create"
-              scrollToFirstError
-              onFinish={handleSubmit}
-              name="validateOnly"
-            >
-              <Form.Item label="Name" name="name" rules={[{ required: true }]}>
-                <Input name="name" />
-              </Form.Item>
-              <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, min: 6 }]}
-              >
-                <Input name="username" />
-              </Form.Item>
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[{ required: true, min: 6 }]}
-              >
-                <Input name="email" />
-              </Form.Item>
-              <Form.Item
-                label="Website"
-                name="website"
-                rules={[{ required: true }]}
-              >
-                <Input name="website" />
-              </Form.Item>
-              <Form.Item
-                label="Phone"
-                name="phone"
-                rules={[{ required: true, min: 6 }]}
-              >
-                <Input name="phone" />
-              </Form.Item>
-              <Form.Item
-                label="Address"
-                name="address"
-                rules={[{ required: true }]}
-              >
-                <TextArea name="address" />
-              </Form.Item>
-              <Form.Item
-                label="Company Name"
-                name="company"
-                rules={[{ required: true }]}
-              >
-                <Input name="company" />
-              </Form.Item>
-              <Form.Item>
-                <Space>
-                  <Button onClick={closeDrawer}>Cancel</Button>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    disabled={!isFormValid}
-                  >
-                    Create User
-                  </Button>
-                </Space>
-              </Form.Item>
-            </Form>
+            <UserForm handleSubmit={handleSubmit} onCancel={closeDrawer} />
           </Drawer>
         </Col>
       </Row>
